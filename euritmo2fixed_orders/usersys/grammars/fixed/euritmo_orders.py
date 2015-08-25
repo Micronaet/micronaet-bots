945fc356d61182dd312cdced1a18713985f1b061
nicothebrush <nicola.riolini@gmail.com>
2015-08-25 16:30:28 +0200

    add BGM record def pep8

--- euritmo2fixed_orders/usersys/grammars/fixed/euritmo_orders.py
+++ euritmo2fixed_orders/usersys/grammars/fixed/euritmo_orders.py
@@ -41,36 +41,40 @@ structure = [
 # the fields in each record. 'BOTSID' is the record tag.
 recorddefs = {
     'BGM': [ # header record
-            ['TIPOREC', 'M', (3, 3), 'A'], # BOTSID
+        ['TIPOREC', 'M', (3, 3), 'AN'], # BOTSID
             
-            # ID-EDI-MITT:
-            ['ID-EDI-MITT-1', 'M', (35, 35), 'AN'], # ID sender Piva o EAN/UCC
-            ['ID-EDI-MITT-2', 'C', (35, 35), 'AN'], 
-            ['ID-EDI-MITT-3', 'C', (35, 35), 'AN'], 
+        # ID-EDI-MITT:
+        ['ID-EDI-MITT-1', 'M', (35, 35), 'AN'], # ID sender Piva o EAN/UCC
+        ['ID-EDI-MITT-2', 'C', (35, 35), 'AN'], 
+        ['ID-EDI-MITT-3', 'C', (35, 35), 'AN'], 
 
-            # ID-EDI-DEST:
-            ['ID-EDI-DEST-1', 'M', (35, 35), 'AN'], # EDI ID sender Piva o EAN / UCC
-            ['ID-EDI-DEST-2', 'C', (4, 4), 'AN'], 
-            ['ID-EDI-DEST-3', 'C', (14, 14), 'AN'], 
+        # ID-EDI-DEST:
+        ['ID-EDI-DEST-1', 'M', (35, 35), 'AN'], # EDI ID sender Piva o EAN / UCC
+        ['ID-EDI-DEST-2', 'C', (4, 4), 'AN'], 
+        ['ID-EDI-DEST-3', 'C', (14, 14), 'AN'], 
             
-            ['TIPODOC', 'M', (6, 6), 'AN'], # ORDERS ORDERSP ORDCHG
-            ['NUMODOC', 'M', (35, 35), 'AN'],
-            ['ORADOC', 'C', (4, 4), 'N'], # HHMM
+        ['TIPODOC', 'M', (6, 6), 'AN'], # ORDERS ORDERSP ORDCHG
+        ['NUMODOC', 'M', (35, 35), 'AN'],
+        ['ORADOC', 'C', (4, 4), 'N'], # HHMM
             
-            ['CODAZION', 'C', (3, 3), 'AN'], 
-            # ORDER: C, CONF: M [R(ifiutato), A(ccettato), M(odificato)]
+        ['CODAZION', 'C', (3, 3), 'AN'], 
+        # ORDER: C, CONF: M [R(ifiutato), A(ccettato), M(odificato)]
             
-            ['FLAGIMPE', 'C', (3, 3), 'AN'], 
-            # ORDER: C, CONF: M [X99 = impegn., blank= non impegn.]
+        ['FLAGIMPE', 'C', (3, 3), 'AN'], 
+        # ORDER: C, CONF: M [X99 = impegn., blank= non impegn.]
  
-            ['TIPORD', 'C', (3, 3), 'AN'],
-            # Blank or 105 = Order (different from D90.1 > 105, D96.a > 220)
-            # 211 Blanket order (open)
-            # 224 Urgent
-            # 226 Call off order (dispos. of delivery)
-            # 231 Confirm order from producer
-            # YA9 Preallocated order
-          ],
+        ['TIPORD', 'C', (3, 3), 'AN'],
+        # Blank or 105 = Order (different from D90.1 > 105, D96.a > 220)
+        # 211 Blanket order (open)
+        # 224 Urgent
+        # 226 Call off order (dispos. of delivery)
+        # 231 Confirm order from producer
+        # YA9 Preallocated order
+        ],
+        
+    'RFF': [
+        ['TIPOREC', 'M', (3, 3), 'AN'], # BOTSID        
+        ]      
 
     'LIN': [ # line record
             ['BOTSID', 'C', 3, 'A'],
