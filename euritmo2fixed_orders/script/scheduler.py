4a04b354eabbaab4603ed3cf163f36bef01ad09f
nicothebrush <nicola.riolini@gmail.com>
2015-10-15 09:36:20 +0200

    History is parametrized, pickle file for manage importation list

--- euritmo2fixed_orders/script/scheduler.py
+++ euritmo2fixed_orders/script/scheduler.py
@@ -21,6 +21,7 @@
 import os
 import time
 import ConfigParser
+import pickle
 from os import listdir
 from os.path import isfile, join
 
@@ -34,19 +35,40 @@ path_in = os.path.expanduser(config.get('EDI', 'path_in'))
 path_history = os.path.expanduser(config.get('EDI', 'path_history'))
 path_out = os.path.expanduser(config.get('EDI', 'path_out'))
 path_bot = os.path.expanduser(config.get('EDI', 'path_bot'))
+try:
+    move_in_history = eval(config.get('EDI', 'move_in_history'))
+except:
+    move_in_history = False
+        
+# Extra service file in local folder:
 log_file = 'log.txt'
+pickle_file = 'pickle.dat'
 
 wait = 60 # sec.
-# Path for log:
-#schedule_log = os.path.expanduser(config.get('LOG', 'schedule_log'))
-#import_log = os.path.expanduser(config.get('LOG', 'import_log'))
 
 run_command = 'python %s --new' % join(
     path_bot, 'bots-engine.py')
 
 log_f = open(log_file, 'a')
-    
-# Function:
+
+# -----------------------------------------------------------------------------    
+#                               Function:
+# -----------------------------------------------------------------------------    
+def pickle_write(data):
+    ''' Write list data on pickle file
+    '''
+    out_f = open(pickle_file, 'wb')
+    pickle.dump(data, out_f)
+    out_f.close()
+
+def pickle_read():
+    ''' Read pickle list
+    '''
+    in_f = open(pickle_file, 'rb')
+    data = pickle.load(in_f) or []
+    in_f.close()
+    return data
+
 def clean(line, replace='?'):
     ''' Remove all non ascii char over 127 bit
     '''
@@ -80,14 +102,22 @@ def log(f, message, type_message='info', echo=True):
         print message
     return True
 
-# Code:
-log(log_f, 'Start conversion')
+# -----------------------------------------------------------------------------
+#                                     Code:
+# -----------------------------------------------------------------------------
+log(log_f, '>> Start conversion')
+previous_order = pickle_read()
+log(log_f, '>> Read pickle list')
+
 file_list = [f for f in listdir(path_in) if isfile(join(path_in, f))]
 
 log(log_f, 
     '%s File found:\n %s' % (len(file_list), file_list), )
 
-for filename in file_list:  
+for filename in file_list:
+    # Test here for log event (not in list comprehension)
+    if filename in previous_order:
+        log(log_f, '>> Jumper yet imported: %s' % filename)    
     file_in = join(path_in, filename)
     file_out = join(path_out, filename)
     file_history = join(path_history, filename)
@@ -105,10 +135,17 @@ for filename in file_list:
 
     log(log_f, '2. End clean: %s' % file_in)
     
-    # Move file in history folder:    
-    os.rename(file_in, file_history)
-    log(log_f, '3. History: %s' % file_history)
-    
+    # Move file in history folder (if setted up):    
+    if move_in_history:
+        os.rename(file_in, file_history)
+        log(log_f, '3. History: %s' % file_history)
+    else:
+        log(log_f, '3. No History: %s' % file_history)
+
+    # Add this order and save in pickle file:
+    previous_order.append(filename)    
+    pickle_write(previous_order)
+    log(log_f, '4. Save pickle file: %s' % pickle_file)
 
 # Force load in bots:
 log(log_f, '>> Wait %s sec.' % wait)
